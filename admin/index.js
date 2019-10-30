abb8ed96feff5fbe2c694e4512ab7f9ad83ed1af
Hugo H. Marinho <hugoharan@gmail.com>
2019-10-29 21:34:50 -0300

    implementa api

--- admin/index.js
+++ admin/index.js
@@ -1,77 +1,74 @@
-require('dotenv').config();
+var express  = require('express');
+var app = express();
+var Firebase = require('firebase');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var multer = require('multer');
+var fs = require("fs");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const firebase = require('firebase');
-const Auth = require('./firebase.js');
-const ejs = require('ejs');
-
-let userLogged;
-const app = express()
-var publicDir = require('path').join(__dirname,'/public');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(publicDir));
-app.use('/', express.static(__dirname + '/www')); // redirect root
-// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-// app.use('/style', express.static(__dirname + '/style/')); // redirect CSS bootstrap
-app.set('view engine', 'ejs');
-
-firebase.auth().onAuthStateChanged((user) => {
-    if(user){
-        userLogged = user
-    } else {
-        userLogged = null
-    }
-})
-
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
-app.post('/createuser', (req, res) => {
-    Auth.SignUpWithEmailAndPassword(req.body.email,req.body.password).then((user) => {
-       if(!user.err){
-          let userData = JSON.parse(user)
-          userData = userData.user
-          Auth.insertUserData(userData).then(() => {
-            res.redirect('/dashboard')
-          })
-       }else{
-          return user.err
-       }
-   })
-  })
-
-app.post('/login', (req, res) => {
-    let getBody = req.body;
-    Auth.SignInWithEmailAndPassword(getBody.email, getBody.password)
-    .then((login) => {
-        if(!login.err){
-            res.redirect('/dashboard')
-        }else{
-            res.redirect('/')
-        }
-    })
-})
-
-app.post('/input', (req, res) => {
-    let {name} = req.body
-    Auth.InputData(name).then(() => {
-        res.render('dashboard')
-    })
-})
-
-app.get('/dashboard', function(req, res){
-    if(userLogged){
-        Auth.GetData().then((data) => {
-            res.render('dashboard', {data});
-        })
-    }else{
-        res.redirect('/')
-    }
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
 });
 
-app.listen(process.env.PORT || 3000)
+Firebase.initializeApp({
+    databaseURL: "https://gerenciando-talentos.firebaseio.com/",
+    serviceAccount: './config.json', //this is file that I downloaded from Firebase Console
+});
+
+var db = Firebase.database();
+var leadsRef = db.ref("leads");
+
+// app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+// app.use('/public/uploads',express.static(__dirname + '/public/uploads'));
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+app.get('/', function (req, res) {
+  res.send('API Gerenciando Talentos')
+})
+
+// create user
+app.post('/api/createLead', function(req, res) {
+   // var userEmail = req.body.user_email;
+      var data = req.body;
+      var date = new Date();
+
+      data.cadastro = date.toLocaleString('pt-br', {timezone: 'Brazil/brt'});
+      data.ip = req.connection.remoteAddress;
+    leadsRef.push(data, function(err) {
+      if (err) {
+        res.send(err)
+      } else {
+        // var key = Object.keys(snapshot.val())[0];
+        // console.log(key);
+        res.json({message: "Success: Lead Save.", result: true});
+      }
+    });
+});
+
+// get users
+app.post('/api/getLeads', function(req, res) {
+    var uid = "vix8Uq7pFNRFQ6gyLxs1";
+  if (uid.length != 20) {
+    res.json({message: "Error: uid must be 20 characters long."});
+  } else {
+    leadsRef.once("value", function(snapshot) {
+      //console.log(snapshot);
+      if (snapshot.val() == null) {
+        res.json({message: "Error: No Lead found", "result": false});
+      } else {
+        res.json({"message":"successfully fetch data", "result": true, "data": snapshot.val()});
+      }
+    });
+  }
+});
+
+app.listen(3000);
+console.log("port is 3000");
